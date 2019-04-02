d793a0d30883153d18135000a6ac77b4aafd052b
aftersnowing <gurcks2@gmail.com>
2019-04-02 12:00:47 +0900

    세번째 커밋

--- codesquad_algorithm.js
+++ codesquad_algorithm.js
@@ -8,3 +8,11 @@ var a = parseInt(input[0]);
 var b = parseInt(input[1]);
 console.log(a+b);
 // 2. 표준입력을 1 2 를 받아서 3을 출력
+
+
+var fs = require('fs');
+var input = fs.readFileSync('/dev/stdin').toString().split(' ');
+var a = Number(input[0]);
+var b = Number(input[1]);
+console.log(a-b);
+// 3. 표준입력 1 2를 받아서 1-2 출력
