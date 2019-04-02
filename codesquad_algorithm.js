fdd51bb7cf547397fdb36aa486b02fcb7e182b4e
aftersnowing <gurcks2@gmail.com>
2019-04-02 11:56:03 +0900

    두번째 커밋

--- codesquad_algorithm.js
+++ codesquad_algorithm.js
@@ -2,4 +2,9 @@ console.log('Hello World!');
 // 1. Hello World!를 출력하세요aa
 
 
-
+var fs = require('fs');
+var input = fs.readFileSync('/dev/stdin').toString().split(' ');
+var a = parseInt(input[0]);
+var b = parseInt(input[1]);
+console.log(a+b);
+// 2. 표준입력을 1 2 를 받아서 3을 출력
