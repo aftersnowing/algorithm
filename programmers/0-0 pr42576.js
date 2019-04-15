6afc225ccf3757e21ee990291697517345847599
aftersnowing <gurcks2@gmail.com>
2019-04-15 23:37:00 +0900

    완주하지 못한 선수

---
+++ programmers/0-0 pr42576.js
@@ -0,0 +1,13 @@
+function solution(participant, completion) {
+    participant.sort();
+    completion.sort();
+    for(let i = 0; i < participant.length; i++){
+        if(participant[i] !== completion[i])
+            return participant[i]
+    }
+}
+// 완주하지 못한 선수
+// participant	completion	return
+// [leo, kiki, eden]	[eden, kiki]	leo
+// [marina, josipa, nikola, vinko, filipa]	[josipa, filipa, marina, nikola]	vinko
+// [mislav, stanko, mislav, ana]	[stanko, ana, mislav]	mislav
